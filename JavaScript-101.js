fa20ec5c000752d49d8b6e3ed444d69a01ff4837
blackguycoding <blackguycoding@gmail.com>
2016-05-26 16:08:56 +0300

    create executable file

---
+++ JavaScript-101.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const workshopper = require('workshopper'),
+      path        = require('path')
+
+function fpath (f) {
+    return path.join(__dirname, f)
+}
+
+workshopper({
+    name        : 'JavaScript-101',
+    title       : 'JavaScript-101',
+    subtitle    : 'Practice your basic JavaScript skills',
+    appDir      : __dirname,
+    menuItems   : [],
+    exerciseDir : fpath('./exercises/')
+})
